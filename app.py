99370cb42266010698ec56e10d66f684ee9610b6
Jenny Aguilar <contact@jaguilarweb.com>
2024-07-12 14:34:17 +0200

    feat: Add model to Todo Class

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(200), nullable=False)
+    #To debbugin
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}  >'
+
+with app.app_context():
+    db.create_all()
 
 #Routes
-
 @app.route('/')
 def index():
-    return render_template('index.html', data=[
-        {
-        'description' : 'Todo 1'
-        },
-        {
-        'description' : 'Todo 2'
-        },
-        {
-        'description' : 'Todo 3'
-        },
-    ])
+    data = Todo.query.all()
+    return render_template('index.html', data)
