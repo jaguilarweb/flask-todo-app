aa487716744eb7d1f1172b2bfc427b6454e476ed
Jenny Aguilar <contact@jaguilarweb.com>
2024-07-15 17:54:36 +0200

    feat: Add todo create route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -20,6 +20,17 @@ with app.app_context():
     db.create_all()
 
 #Routes
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    description = request.form.get('description', '')
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
 @app.route('/')
 def index():
-    return render_template('index.html', data = Todo.query.all())
+    return render_template('index.html', data=Todo.query.all())
+
+
