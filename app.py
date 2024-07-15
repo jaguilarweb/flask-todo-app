0a608f5048129147f12e84f0d36cba5f02872025
Jenny Aguilar <contact@jaguilarweb.com>
2024-07-15 19:34:20 +0200

    refactor: Change to add try-except-finally pattern

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import sys
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
@@ -22,13 +23,23 @@ with app.app_context():
 #Routes
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.get_json()['description']
-    todo = Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    return jsonify({
-        'description': todo.description
-    })
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if not error:
+        return jsonify(body)
 
 
 @app.route('/')
